389c00eff868afefcab315ad026e3862e9bf911c
hypnotz <iv.soliso@gmail.com>
2022-02-11 02:15:31 -0300

    "react-router-dom": "^5.2.0",

---
+++ src/components/PokeInfo.js
@@ -0,0 +1,10 @@
+
+const PokeInfo = () => {
+    return (
+   
+    <h1>XDDDDD</h1>
+   
+    )
+}
+
+export default PokeInfo;
--- src/components/componentePrueba.js
+++ src/components/componentePrueba.js
@@ -1,15 +1,5 @@
 import * as React from "react";
-import { experimentalStyled as styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 export default function ComponentePrueba() {
   return <></>;
